72432e42efc8243cd5285afc98949f220b8fcfae
carrotpai <55906326+carrotpai@users.noreply.github.com>
2023-01-10 16:53:46 +0800

    account.sol work

--- contracts/account.sol
+++ contracts/account.sol
@@ -5,12 +5,21 @@ import "hardhat/console.sol";
 
 contract Account {
     uint constant DURATION = 7 days;
+
+    event Received(address, uint);
+
+    receive() external payable {
+        emit Received(msg.sender, msg.value);
+        console.log("received: %s", address(this).balance);
+    }
+
     struct Player {
         string nickname;
         uint balance;
         uint rating;
         uint lastTimeClaimed;
         bool created;
+        bool participant;
     }
 
     address public faceitOwner;
@@ -20,6 +29,13 @@ contract Account {
     constructor() {
         faceitOwner = msg.sender;
         console.log("Who is calling ctor: %s", msg.sender);
+    }
+
+    function participate() public payable {
+        //require(balances[msg.sender].created)
+        balances[msg.sender].participant = true;
+
+        console.log("total sum on contract: %s", address(this).balance);
     }
 
     function createPlayerAccount(
@@ -33,7 +49,8 @@ contract Account {
             balance: 0,
             rating: _rating,
             lastTimeClaimed: block.timestamp,
-            created: true
+            created: true,
+            participant: false
         });
 
         balances[msg.sender] = newPlayer;
@@ -45,6 +62,8 @@ contract Account {
 
     function balanceAccrual(uint _rating) public {
         //require(block.timestamp - balances[msg.sender].lastTimeClaimed >= DURATION);
+        //require(balances[msg.sender].created)
+        //require(balances[msg.sender].participant)
         balances[msg.sender].balance += 1;
         console.log(
             "account %s --- new Balance: %s with rating %s",
@@ -52,10 +71,13 @@ contract Account {
             balances[msg.sender].balance,
             _rating
         );
+        uint value = 1 wei;
         address payable _to = payable(msg.sender);
+        require(address(this).balance > value);
+        _to.transfer(value);
     }
 
-    function getETHforRating(uint _rating) private pure returns (uint) {
-        return 1;
+    function getETHforRating(uint _rating) private view returns (uint) {
+        return (balances[msg.sender].rating - _rating);
     }
 }
